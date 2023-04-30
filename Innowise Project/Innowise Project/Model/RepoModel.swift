c5f5ab98963060bea11c804d3c377afe5152eafd
Sinbanbonn <logvinov0607@icloud.com>
2023-05-01 00:17:03 +0300

    adding new fields

--- Innowise Project/Innowise Project/Model/RepoModel.swift
+++ Innowise Project/Innowise Project/Model/RepoModel.swift
@@ -3,19 +3,27 @@ import UIKit
 
 
 struct RepoModel{
-    let name: String
+    let name : String
+    let title: String
     let description: String
     let picture: String
     let source : String
+    let htmlURL : String
+    let type : String
     var image: UIImage?
- 
-    init(name: String, description: String, picture: String, source: String, image: UIImage? = nil) {
+    
+    init(name: String, title: String, description: String, picture: String, source: String, htmlURL: String, type: String, image: UIImage? = nil) {
         self.name = name
+        self.title = title
         self.description = description
         self.picture = picture
         self.source = source
+        self.htmlURL = htmlURL
+        self.type = type
         self.image = image
     }
+ 
+   
     
     
 }
